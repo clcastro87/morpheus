fec701949f09b6c3f1b5bc010d06f18372941329
Carlos Castro <carlos_luis@artex.cu>
2018-01-31 10:42:54 -0800

    Removing 404 dependency to config. Setting up debug for 404 middleware.

--- src/lib/notFound.js
+++ src/lib/notFound.js
@@ -3,25 +3,29 @@
  * Should be the last middleware in the pipeline
  *
  * @type {fourOFour}
+ * @author: Carlos Luis Castro Márquez
+ * 
  ************************/
 
-var logger = require('./logger')(module);
 var response = require('./response');
-var environment = require('./config').environment;
+const DEBUG_SIGNATURE = 'morpheus.middleware.404';
+var debug = require('debug')(DEBUG_SIGNATURE);
+var environment = (process.env && process.env.NODE_ENV) || 'development';
 
 module.exports = fourOFour;
 
 function fourOFour(req, res) {
-    // Checks if headers are already sent
+    // Checks if headers was already sent
     if (res.headersSent) {
         return res.end();
     }
     // If not then build 404 response.
     var internalMessage = 'Unable to find route: ' + req.url;
+    debug(internalMessage);
+    // Transform message in development to find out route error.
     var message = 'Not Found';
     if ('development' === environment) {
         message = internalMessage;
-        logger.error(internalMessage);
     }
     res.status(404).send(response.error(404, message)).end();
 }
